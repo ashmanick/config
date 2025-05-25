384e071fbc5396e18338530dc91f8aa74eb502e9
n.ashmarov <ashmanicks@gmail.com>
2025-05-25 21:24:09 +0300

    fix tab key

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,5 +8,11 @@ end
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+
+o.tabstop = 4 -- 2 spaces for tabs (prettier default)
+o.shiftwidth = 4 -- 2 spaces for indent width
+o.expandtab = true -- expand tab to spaces
+o.autoindent = true -- copy indent from current line when starting new one
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
